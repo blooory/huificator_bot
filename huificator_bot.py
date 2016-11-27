234f9bf37f2d80c24583f8644a1a9e80647367de
bixic <poteriaev@gmail.com>
2016-11-27 14:23:08 +0300

    some changes add osel case

--- huificator_bot.py
+++ huificator_bot.py
@@ -6,7 +6,8 @@ import numpy as np
 import json
 import pickle
 from special_function import special_function
-from word_functions import n_slogov, vowel_pos, replace_string, leave_only_russian_letters, huificate_word, preprocess_text, find_verbs
+from word_functions import n_slogov, vowel_pos, replace_string, leave_only_russian_letters, huificate_word, \
+                           preprocess_text, find_verbs
 import pymorphy2 as pm2
 import csv
 
@@ -14,26 +15,39 @@ with open('params.json', 'r') as temp_file:
     TOKEN = json.load(temp_file)['TOKEN']
 
 # Add word dictionary
-word_dict={}
+word_dict = {}
 with open('dict.txt') as f:
-    word_dict={key:value for (key,value) in csv.reader(f)}
+    word_dict = {key: value for (key, value) in csv.reader(f)}
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
 
 def start(bot, update):
     update.message.reply_text('Хуй тебе {0}, рад видеть'.format(update.message.from_user.first_name))
     with open('chat_ids.txt', 'a') as f:
-        f.write(str(update.message.chat_id) +';'+ update.message.from_user.first_name + ';'+ update.message.from_user.last_name + ';' + str(update.message.date) +'\n')
+        f.write(str(update.message.chat_id) + ';' +
+                update.message.from_user.first_name + ';' +
+                update.message.from_user.last_name + ';' +
+                str(update.message.date) + '\n')
+
 
 def hui(bot, update):
     rv = np.random.rand(1)[0]
+
+    # запоминаем имя автора сообщения
+    name_user = update.message.from_user.first_name
+
     # say smth to net
-    if '?' in update.message.text:
-        if rv<0.05:
-            bot.sendMessage(chat_id = update.message.chat_id, text = 'Отвечают только пидарасы')
+
+    if ('почему' in str.lower(update.message.text)) & (' почему' not in str.lower(update.message.text)):
+        bot.sendMessage(chat_id=update.message.chat_id, text='Потому что ты осёл, ' + name_user)
+    elif '?' in update.message.text:
+        if rv < 0.05:
+            bot.sendMessage(chat_id=update.message.chat_id, text='Отвечают только пидарасы')
     elif leave_only_russian_letters(str.lower(update.message.text)) in word_dict:
-        if rv<0.35:
-            bot.sendMessage(chat_id = update.message.chat_id, text = word_dict[leave_only_russian_letters(str.lower(update.message.text))])
+        if rv < 0.35:
+            bot.sendMessage(chat_id=update.message.chat_id,
+                            text=word_dict[leave_only_russian_letters(str.lower(update.message.text))])
     else:
         # huificate word
         if rv < 0.2:
@@ -41,30 +55,26 @@ def hui(bot, update):
             if len(text) != 0:  
                 flag, hui_word = special_function(text, update)
                 if flag:
-                    bot.sendMessage(chat_id = update.message.chat_id, text = hui_word)
-                elif (rv<0.07):        
+                    bot.sendMessage(chat_id=update.message.chat_id, text=hui_word)
+                elif (rv < 0.07):
                     hui_word = huificate_word(np.random.choice(text, 1)[0])
-                    bot.sendMessage(chat_id = update.message.chat_id, text = hui_word)
+                    bot.sendMessage(chat_id=update.message.chat_id, text=hui_word)
         # say smth about mom
-        if (rv > 0.925):
+        if rv > 0.925:
             print('for mamka', update.message.text, update.message.chat_id)
             text = preprocess_text(update.message.text)
-            if (len(text) != 0) :
+            if len(text) != 0:
                 verbs = find_verbs(text, parser)
                 if len(verbs) != 0:
-                    name_user = update.message.from_user.first_name
-                    your_mom_message = 'Мамка твоя '+np.random.choice(verbs,1)[0]+', '+name_user+'!'
+                    your_mom_message = 'Мамка твоя '+np.random.choice(verbs, 1)[0]+', '+name_user+'!'
                     print(your_mom_message)
-                    bot.sendMessage(chat_id = update.message.chat_id, text = your_mom_message)
-    
-
+                    bot.sendMessage(chat_id=update.message.chat_id, text=your_mom_message)
 
 updater = Updater(TOKEN)
 
 parser = pm2.MorphAnalyzer()
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler([Filters.text], hui))
-
 
 updater.start_polling()
 updater.idle()
--- word_functions.py
+++ word_functions.py
@@ -1,22 +1,26 @@
 import string
 
+
 def n_slogov(s):
     return len(list(filter(lambda x: x in 'уеыаоэяиюё', list(s))))
+
 
 def vowel_pos(s, num_vowel):
     cnt = 0
     vowel_cnt = 0
     for i in s:
-        if (i in 'уеыаоэяиюё'):
+        if i in 'уеыаоэяиюё':
             vowel_cnt += 1
             if vowel_cnt == num_vowel:
                 return cnt
         cnt += 1
 
+
 def replace_string(s, d):
     for key in d:
         s = s.replace(key, d[key])
     return s
+
 
 def leave_only_russian_letters(s):
     s_upd = ''
@@ -24,26 +28,29 @@ def leave_only_russian_letters(s):
         s_upd += letter*(letter in 'ёйцукенгшщзхъфывапролджэячсмитьбю ')
     return s_upd
 
+
 def huificate_word(hui_word):
     prefix = 'ху'
     print(hui_word)
     if n_slogov(hui_word) <= 2:
         vowel = hui_word[vowel_pos(hui_word, 1)]
         postfix = hui_word[(vowel_pos(hui_word, 1)+1):]
-        vowel = replace_string(vowel, {'а':'я', 'у':'ю', 'о':'ё','э':'е'})
+        vowel = replace_string(vowel, {'а': 'я', 'у': 'ю', 'о': 'ё', 'э': 'е'})
     else:
         vowel = hui_word[vowel_pos(hui_word, 2)]
         postfix = hui_word[(vowel_pos(hui_word, 2)+1):]
-        vowel = replace_string(vowel, {'а':'я', 'у':'ю', 'о':'ё','э':'е'})
+        vowel = replace_string(vowel, {'а': 'я', 'у': 'ю', 'о': 'ё', 'э': 'е'})
     return prefix + vowel + postfix
+
 
 def preprocess_text(text):
     exclude = set(string.punctuation)
     text = (''.join(ch for ch in text if ch not in exclude)).lower()
     text = leave_only_russian_letters(text)
-    text = [x for x in (set(text.split(' ')) - {''}) if n_slogov(x)>=2]
+    text = [x for x in (set(text.split(' ')) - {''}) if n_slogov(x) >= 2]
     return text
-    
+
+
 def find_verbs(words, parser):
     verbs = []
     COUNT = 'sing'
@@ -53,12 +60,11 @@ def find_verbs(words, parser):
         res = parser.parse(word)[0]
         if (res.tag.POS == 'VERB') | (res.tag.POS == 'INFN'):
             if res.tag.gender is None:
-                res = res.inflect({COUNT,PERSON})
+                res = res.inflect({COUNT, PERSON})
             else:
-                res = res.inflect({COUNT,PERSON,GENDER})
+                res = res.inflect({COUNT, PERSON, GENDER})
             try:
                 verbs.append(res.word)
             except:
                 pass
     return verbs
-    
